a983391ff62561d4d6a1e04e60c609d7e02c5cfd
Tike <124289043+TarasKostr@users.noreply.github.com>
2025-09-21 20:06:30 +0300

    Updates

--- parceltest.0f77c784.js
+++ parceltest.0f77c784.js
@@ -207,11 +207,11 @@
       });
     }
   }
-})({"7wZbQ":[function(require,module,exports,__globalThis) {
+})({"26vNV":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 1234;
+var HMR_SERVER_PORT = 62731;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "439701173a9199ea";
 var HMR_USE_SSE = false;
@@ -716,6 +716,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"2R06K":[function(require,module,exports,__globalThis) {
 console.log("Hello world, goodbye sanity");
 
-},{}]},["7wZbQ","2R06K"], "2R06K", "parcelRequire49e2", {})
+},{}]},["26vNV","2R06K"], "2R06K", "parcelRequire49e2", {})
 
 //# sourceMappingURL=parceltest.0f77c784.js.map
